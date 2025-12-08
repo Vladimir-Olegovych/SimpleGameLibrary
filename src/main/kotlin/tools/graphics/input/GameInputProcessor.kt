5e7b72202fa37355d77e7cd0e6f7eddf9f5436af
vladimir <kuzvenkov.vladimir@gmail.com>
2025-12-08 20:12:14 +0400

    draw optimization commit

---
+++ src/main/kotlin/tools/graphics/input/GameInputProcessor.kt
@@ -0,0 +1,19 @@
+package tools.graphics.input
+
+import com.badlogic.gdx.InputProcessor
+
+interface GameInputProcessor : InputProcessor {
+
+    fun onPause() {}
+    fun onResume() {}
+
+    override fun keyDown(keycode: Int): Boolean = false
+    override fun keyUp(keycode: Int): Boolean = false
+    override fun keyTyped(character: Char): Boolean = false
+    override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean = false
+    override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean = false
+    override fun touchCancelled(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean = false
+    override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean = false
+    override fun mouseMoved(screenX: Int, screenY: Int): Boolean = false
+    override fun scrolled(amountX: Float, amountY: Float): Boolean = false
+}
